9533be67e39295000647694a06407b39814101d0
immortaltw <ian.immortal@gmail.com>
2016-04-18 23:56:33 +0800

    placeholders

---
+++ include/EstimateVars.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+struct EstimateVars {
+	int interval;       // Observation Interval
+	double x0;			// Approximate X location
+	double y0;			// Approximate Y location
+	double z0;			// Approximate Z location
+	double startTime;   // Observation start time
+	double currTime;    // Current observation time
+	double pseudoRange; // Use L1 code phase pseudo range only
+	std::vector<std::string> PRNS;  // PRN names
+};
